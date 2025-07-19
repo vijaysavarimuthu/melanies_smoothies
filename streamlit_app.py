79f0e0db83352ea70141cf94f9254f47fca6710a
vijaysavarimuthu <amalivijay@gmail.com>
2025-07-19 21:35:03 +0530

    Update streamlit_app.py
    added display json text in the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
 # Write directly to the app
 st.title(f":cup_with_straw:**Customize Your Smoothie**:cup_with_straw:")
 st.write(
@@ -51,5 +48,7 @@ VALUES (DEFAULT, DEFAULT, '{name_on_order}', '{ingredients_string}', DEFAULT);
     #session = get_active_session()
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)  
+    st.text(smoothiefroot_response.json())
         st.success('Your Smoothie is ordered!', icon="✅")
