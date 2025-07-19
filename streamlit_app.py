672cfb53b2346f4d6da84fe1ab4d15df01ca158d
vijaysavarimuthu <amalivijay@gmail.com>
2025-07-19 21:47:30 +0530

    Update streamlit_app.py
    added dataframe api

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,4 +51,5 @@ VALUES (DEFAULT, DEFAULT, '{name_on_order}', '{ingredients_string}', DEFAULT);
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     st.text(smoothiefroot_response)  
     st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     st.success('Your Smoothie is ordered!', icon="✅")
