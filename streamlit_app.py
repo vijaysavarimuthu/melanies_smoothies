eb49ff9f3f60c8708d30d6dcb2fe970baf656767
vijaysavarimuthu <amalivijay@gmail.com>
2025-07-19 21:49:40 +0530

    Update streamlit_app.py
    api issue fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,10 @@ ingredients_list = st.multiselect(
 )
 
 #if ingredients_list is not null
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)  
-    st.text(smoothiefroot_response.json())
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)  
+st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 if ingredients_list:
     # st.write(ingredients_list)
     # st.text(ingredients_list)
