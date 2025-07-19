52751d6069f1ea6e60516a1cb8ceaee57565d47a
vijaysavarimuthu <amalivijay@gmail.com>
2025-07-19 21:54:04 +0530

    Update streamlit_app.py
    updated alternate api data

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ ingredients_list = st.multiselect(
 )
 
 #if ingredients_list is not null
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
 st.text(smoothiefroot_response)  
 st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
